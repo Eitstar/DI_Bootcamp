52f17b3f0b1584d1e1f6013cfe95fb2a22e179c1
eitstar <eitanmarks25@gmail.com>
2021-04-11 16:29:36 +0300

    week2-day1

---
+++ week 2/D1/Exercises-XP.js
@@ -0,0 +1,106 @@
+// Exercise 1 : Guess The Answers
+
+
+// 5 + "34"    
+// my  Guess:
+// thinking it should be as a mathamaticl & calculate meaning  add 5 to the "34" = "39"
+// the correct answer is:
+// "534"
+
+
+
+// 5 - "4" 
+// my  Guess:
+// thinking it should be as a mathamaticl & calculate meaning  reduce 5 from "4" = "1"
+// the correct  answer is: 
+// the correct answer is the same as my guess was
+
+
+
+//      10 % 5  
+// my  Guess:
+// thinking it should come-out  0.5
+// the correct answer is:
+// 0 
+
+
+
+//  5 % 10 
+// my  Guess:
+// pretty much the same as before 
+// the correct  answer is:
+// 5
+// +++ apprently JavaScript Works in the way of Modulus (Remainder) with %
+
+
+
+
+//     "Java" + "Script" 
+// my  Guess:
+// "JavaScript"  
+// the correct  answer is: 
+// the correct answer is the same as my guess was
+
+
+
+//     " " + " "  
+// my  Guess:
+// i wasn't sure  what the answer should be
+// the correct  answer is: 
+// " "
+// every string via + sign to will add to the other string,  in this case space + space in a string = one space " "
+
+
+
+//     " " +  0 
+// my  Guess:
+// will be " 0"  
+// the correct  answer is: 
+// the correct answer is the same as my guess was
+
+
+
+
+//     true + true   
+// my  Guess: 
+// BCZ true in JavaScript =1 & false =0 the answer should be 2
+// the correct  answer is: 
+// the correct answer is the same as my guess was
+
+
+//     true + false
+// my  Guess:
+// BCZ true in JavaScript =1 & false =0 the answer should be 1
+// the correct  answer is: 
+// the correct answer is the same as my guess was
+
+
+
+
+//     false + true
+// my  Guess:
+// same as the previous  
+
+
+
+//     false - true
+// my  Guess:
+// should be -1
+// the correct  answer is: 
+// the correct answer is the same as my guess was
+
+
+
+//     3 - 4
+// my  Guess:
+// should be -1
+// the correct  answer is: 
+// the correct answer is the same as my guess was
+
+
+//     "Bob" - "bill"
+// my  Guess: 
+//Nan 
+// the correct  answer is: 
+// The global NaN property is a value representing Not-A-Number.
+// when the string isnt a numaric ithis shuld be the answer
