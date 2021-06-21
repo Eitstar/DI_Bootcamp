4b2dc25c43ede5b05cccd06112e9e839d8f62d42
eitstar <eitanmarks25@gmail.com>
2021-06-21 18:38:29 +0300

    7a

--- week 7/D3/xp/fs.js
+++ week 7/D3/xp/fs.js
@@ -2,7 +2,7 @@ const fs = require('fs')
     //Create a text file and write something inside (example: ‘Some Text To See’)
     // Create an fs.js file, and inside use the 
     // Node JS File System to read the text file and console.log the output in the terminal
-fs.readFile('./text.txt', 'utf8', (err, data) => {
+fs.readFile('./text.text', 'utf8', (err, data) => {
         if (err) return console.error(err)
 
         console.log(data)
@@ -24,16 +24,16 @@ fs.readFile('./text.txt', 'utf8', (err, data) => {
 // Create an fs.js file, and use the Node js File System to create and write to a new text file. (Example: “Buy Milk”)
 
 
-fs.writeFile('./text3.txt', ('text3, added from writeFile'), err => {
+fs.writeFile('./text3.text', ('text3, added from writeFile'), err => {
     if (err) return console.error(err)
 })
 
-fs.appendFile('./text3.txt', ' this is append data', function(err) {
+fs.appendFile('./text3.text', ' this is append data', function(err) {
     if (err) throw err;
     console.log('text3!');
 });
 
-fs.unlink('./text3.txt', function(err) {
+fs.unlink('./text3.text', function(err) {
     if (err) throw err;
     console.log('text3 deleted!');
 });
