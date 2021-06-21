844904fc6917b43e1dd5aba1f1dce48cd946e597
eitstar <eitanmarks25@gmail.com>
2021-06-21 18:30:46 +0300

    7

---
+++ week 7/D3/xp/fs.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+    //Create a text file and write something inside (example: ‘Some Text To See’)
+    // Create an fs.js file, and inside use the 
+    // Node JS File System to read the text file and console.log the output in the terminal
+fs.readFile('./text.txt', 'utf8', (err, data) => {
+        if (err) return console.error(err)
+
+        console.log(data)
+    })
+    //Create an fs.js file, and use the Node js File System to create a new text file and write to it.
+
+
+
+// fs.writeFile('./text2.txt', ('text2, added from writeFile'), err => {
+//     if (err) return console.error(err)
+
+
+//file written successfully
+//   })
+
+
+//   Exercise 3 : Appending And Deleting Files With Node JS
+// Instructions
+// Create an fs.js file, and use the Node js File System to create and write to a new text file. (Example: “Buy Milk”)
+
+
+fs.writeFile('./text3.txt', ('text3, added from writeFile'), err => {
+    if (err) return console.error(err)
+})
+
+fs.appendFile('./text3.txt', ' this is append data', function(err) {
+    if (err) throw err;
+    console.log('text3!');
+});
+
+fs.unlink('./text3.txt', function(err) {
+    if (err) throw err;
+    console.log('text3 deleted!');
+});
